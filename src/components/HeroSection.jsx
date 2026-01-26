2fb22637bdcd69ba1c32ebbc191d7965eb4f06b5
Abelhad <hassanabelhad51@gmail.com>
2026-01-26 03:32:13 +0100

    hero section first design commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+const HeroSection = () => {
+  return (
+    <div className="heroSection">
+        <div className="customHeader">
+            <i class="fa-regular fa-clock" id="timerLogo"></i>
+            <h2>Pomodoro</h2>
+        </div>
+        <div className="clock">
+            
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
