c3475797267d34df33b2aceb17ee366db5eefddf
Abelhad <hassanabelhad51@gmail.com>
2026-01-27 04:47:47 +0100

    updating design

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react"
+
 const HeroSection = () => {
+  let [minute, setMinute] = useState(25);
+  let [second, setSecond] = useState(0);
+  let [timerState, setTimerState] = useState("Start");
   return (
     <div className="heroSection">
         <div className="customHeader">
-            <i class="fa-regular fa-clock" id="timerLogo"></i>
-            <h2>Pomodoro</h2>
+            <img src="https://www.tomatotimers.com/assets/images/tomato.svg" alt="" />
         </div>
+        <div className="TimeProgress"></div>
         <div className="clock">
-            
+            <div className="timerType">
+              <button>Pomodoro</button>
+              <button>Short Break</button>
+              <button>Long Break</button>
+            </div>
+            <div className="actualTimer">
+              <h3>{minute}</h3>
+              <h3>:</h3>
+              <h3>{second}0</h3>
+            </div>
+            <div className="timerStateButton">
+              <button>{timerState}</button>
+            </div>
         </div>
     </div>
   )
