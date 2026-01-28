63a03834c16bb544e299af3cd4f4a036a957e095
Abelhad <hassanabelhad51@gmail.com>
2026-01-28 04:06:52 +0100

    last design update before logic

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,19 +11,30 @@ const HeroSection = () => {
         </div>
         <div className="TimeProgress"></div>
         <div className="clock">
-            <div className="timerType">
-              <button>Pomodoro</button>
-              <button>Short Break</button>
-              <button>Long Break</button>
-            </div>
-            <div className="actualTimer">
-              <h3>{minute}</h3>
-              <h3>:</h3>
-              <h3>{second}0</h3>
-            </div>
-            <div className="timerStateButton">
-              <button>{timerState}</button>
-            </div>
+          <div className="timerType">
+            <button>Pomodoro</button>
+            <button>Short Break</button>
+            <button>Long Break</button>
+          </div>
+          <div className="actualTimer">
+            <h3>{minute}</h3>
+            <h3>:</h3>
+            <h3>{second}0</h3>
+          </div>
+          <div className="timerStateButton">
+            <button>{timerState}</button>
+          </div>
+        </div>
+        <div className="currentTask">
+          <h5>#1</h5>
+          <h4>task name</h4>
+        </div>
+        <div className="titlesHeader">
+          <h3>Tasks</h3>
+          <i class="fa-solid fa-list"></i>
+        </div>
+        <div className="addTaskButton">
+          <button><i class="fa-solid fa-plus"></i> Add Task</button>
         </div>
     </div>
   )
