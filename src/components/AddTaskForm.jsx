159d1d24e6b9a4c63310fa251474fb226c0642c1
Abelhad <hassanabelhad51@gmail.com>
2026-02-09 19:02:14 +0100

    final task form design

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,7 +1,21 @@
-const AddTaskForm = () => {
+import { useState } from "react"
+
+const AddTaskForm = ( {addTask} ) => {
+  let [estPomodoros, setEstPomodoros] = useState(1);
   return (
     <div className="TaskForm">
-
+      <form action="" onSubmit={addTask}>
+        <div className="inputs">
+          <input type="text" id="taskTitle" autoFocus placeholder="What are you working on ?"/>
+          <div>
+            <label htmlFor="">Est Pomodoros</label>
+            <input type="number" class="num-input" value={estPomodoros} onChange={(e)=>{setEstPomodoros(e.target.value)}}/>
+          </div>
+        </div>
+        <div className="saveButtonCont">
+          <button type="submit">save</button>
+        </div>
+      </form>
     </div>
   )
 }
